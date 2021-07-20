f2d7c96a13a607f1e9d494f9ad35efc236c54475
Jay Solanki <jaysolanki46@gmail.com>
2021-07-21 11:37:05 +1200

    Home screen header design

---
+++ config/Headertext.js
@@ -0,0 +1,28 @@
+import Colors from "./Colors";
+
+export default {
+    h1: {
+        fontSize: 35,
+        fontWeight: 'bold',
+        color: Colors.darkFont
+    },
+    h2: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: Colors.darkFont
+    },
+    h3: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: Colors.darkFont
+    },
+    h4: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: Colors.darkFont
+    },
+    h5: {
+        fontSize: 15,
+        color: Colors.darkFont
+    },
+}
--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet, Text, View, Image, StatusBar } from 'react-native';
 import Colors from '../config/Colors';
+import Headertext from '../config/Headertext';
 
 export default Home = () => {
     return (
-        <SafeAreaView style={styles.container}>
-        <Text>This is top text.</Text>
-        <Text>This is bottom text.</Text>
-        </SafeAreaView>
+        <View style={styles.container}>
+            {Platform.OS === 'ios' && <>
+            <StatusBar barStyle="dark-content" hidden = {false} translucent = {true}></StatusBar>
+            </>}
+            <View style={styles.header}>
+            <View style={styles.headerSubView}>
+                    <View style={styles.headerLeft}>
+                        <Text style={Headertext.h1}>Hey,</Text>
+                        <Text style={Headertext.h4}>Welcome back, Post Tech!</Text>
+                    </View>
+                    <View style={styles.headerRight}>
+                        <Image
+                            style={styles.tinyLogo}
+                            source={{
+                            uri: 'https://reactnative.dev/img/tiny_logo.png',
+                            }}
+                        />
+                    </View>
+                </View>
+            </View>
+            <View style={styles.body}>
+
+            </View>
+        </View>
     );
 }
 
@@ -16,9 +36,44 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       paddingTop: Platform.OS === 'ios'? 10 : 60,
-      paddingBottom: 10,
       alignItems: 'center',
       justifyContent: 'center',
-      backgroundColor: Colors.white,
+      backgroundColor: "#e7e4d1",
+      flexDirection: 'column',
+    },
+    header: {
+        flex: 1.5,
+        alignItems: 'center',
+        width: '100%',
+    },
+    body: {
+        flex: 4.5,
+        alignItems: 'center',
+        backgroundColor: "white",
+        width: '100%',
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40,
+        flexDirection: "row",
+        alignContent: "space-between",
+    },
+    headerSubView: {
+        flex: 1, 
+        flexDirection: 'row',
+        margin: 10, 
+    },
+    headerLeft: {
+        flex: 4,
+        width: '20%',
+        justifyContent: 'center',
+    },
+    headerRight: {
+        flex: 2,
+        width: '20%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    tinyLogo: {
+        width: 75,
+        height: 75,
     },
 });

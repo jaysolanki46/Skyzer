bf73919ebf54b67099b5485c18b5a869c1e453bc
Jay Solanki <jaysolanki46@gmail.com>
2021-07-26 16:55:54 +1200

    Profile: Image picker

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ const App = () => {
      
       <NavigationContainer>
         <Tab.Navigator
-          initialRouteName="Profile"
+          initialRouteName="Home"
           activeColor={Colors.active}
           inactiveColor={Colors.inactive}
           labelStyle={{ fontSize: 12 }}
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View, StatusBar, Button,TouchableOpacity  } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, StatusBar, Button,Image  } from 'react-native';
 import Colors from '../config/Colors';
 import Headertext from '../config/Headertext';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Menu, Divider, Provider } from 'react-native-paper';
+import * as ImagePicker from 'expo-image-picker';
 
 export default Profile = () => {
+
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+      (async () => {
+        if (Platform.OS !== 'web') {
+          const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+          if (status !== 'granted') {
+            alert('Sorry, we need camera roll permissions to make this work!');
+          }
+        }
+      })();
+    }, []);
+  
+    const pickImage = async () => {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+  
+      console.log(result);
+  
+      if (!result.cancelled) {
+        setImage(result.uri);
+      }
+    };
 
     return (
         <View style={styles.container}>
@@ -21,7 +48,10 @@ export default Profile = () => {
                 <View style={styles.bodySubView}>
                     <View style={styles.bodyTop}>
                         <View style={styles.bodyTopLeft}>
-        
+                            {image && <Image 
+                                style={styles.tinyLogo}
+                                source={{ uri: image }} />}
+                            <Button title="Edit" onPress={pickImage} />
                         </View>
                         <View style={styles.bodyTopRight}>
 
@@ -80,28 +110,35 @@ const styles = StyleSheet.create({
     },
     bodyTop: {
         borderWidth: 1,
-        flex: 1,
+        flex: 2,
         flexDirection: 'row',
         backgroundColor: Colors.white,
     },
     bodyTopLeft: {
         borderWidth: 1,
-        flex: 3,
+        flex: 2,
         backgroundColor: Colors.yellow,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     bodyTopRight: {
         borderWidth: 1,
-        flex: 3,
+        flex: 4,
         backgroundColor: Colors.green,
     },
     bodyMiddle: {
         borderWidth: 1,
-        flex: 4,
+        flex: 3,
         backgroundColor: Colors.white,
     },
     bodyBottom: {
         borderWidth: 1,
         flex: 1,
         backgroundColor: Colors.green,
+    },
+    tinyLogo: {
+        width: 75,
+        height: 75,
+        borderRadius: 50,
     },
 });

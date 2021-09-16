539d61da2c2391829533bae30703ed451abae9a0
Jay Solanki <Jay.Solanki@nz.skyzer.local>
2021-09-16 12:41:09 +1200

    Contact with link

--- screens/Contact.js
+++ screens/Contact.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { StyleSheet, Platform, StatusBar, SafeAreaView, Text, View, Image } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { StyleSheet, Platform, StatusBar, SafeAreaView, Text, View, Image, Animated, Alert, TouchableOpacity, Linking } from 'react-native';
 import Colors from '../config/Colors';
 import contactImage from '../assets/images/contact.png';
 import phone from '../assets/images/contact-phone.png';
 import email from '../assets/images/contact-email.png';
 import Headertext from '../config/Headertext';
+
+const LRView = (props) => {
+    const LRanim = useRef(new Animated.Value(0)).current  // Initial value for opacity: 0
+
+    React.useEffect(() => {
+        Animated.timing(
+            LRanim,
+            {
+                toValue: 1,
+                duration: 2000,
+                useNativeDriver: true,
+            }
+        ).start();
+    }, [LRanim])
+
+    return (
+        <Animated.View
+            style={{
+                ...props.style,
+                opacity: LRanim,
+            }}
+        >
+            {props.children}
+        </Animated.View>
+    );
+}
 
 export default Contact = ({ navigation }) => {
 
@@ -25,30 +51,34 @@ export default Contact = ({ navigation }) => {
 
             <View style={styles.body}>
                 <View style={styles.bodyEmail}>
+                    <TouchableOpacity onPress={() => { Linking.openURL(`mailto:support@skyzer.co.nz`) }}>
                     <View style={styles.cardEmail}>
                         <Text style={{ color: Colors.fontColorWhite, fontWeight: 'bold', fontSize: 17, marginLeft: 'auto', margin: 10}}>
                             support@skyzer.co.nz
                         </Text>
                         <Image
-                            style={{width: 50, height: 50, marginLeft: 'auto', marginRight: 20}}
+                            style={{ width: 50, height: 50, borderRadius: 10, marginLeft: 'auto', marginRight: 10}}
                             source={email}
                         />
                     </View>
+                    </TouchableOpacity>
                 </View>
                 <View style={styles.bodyPhone}>
+                    <TouchableOpacity onPress={() => { Linking.openURL(`tel:092590322`) }}>
                     <View style={styles.cardPhone}>
                         <Image
-                            style={{ width: 40, height: 40, marginRight: 'auto', marginLeft: 20}}
+                            style={{ width: 50, height: 50, borderRadius: 10, marginRight: 'auto', marginLeft: 10}}
                             source={phone}
                         />
                         <Text style={{ color: Colors.fontColorWhite, fontWeight: 'bold', fontSize: 17, marginRight: 'auto', margin: 10 }}>
-                            0800 252 252
+                            09 259 0322
                         </Text>
                     </View>
+                    </TouchableOpacity>
                 </View>
             </View>
             
-            <View style={styles.footer}>
+            <LRView style={styles.footer}>
                 <View style={{borderBottomWidth: 1, borderBottomColor: Colors.seperator, 
                     borderBottomRightRadius: 10, borderBottomLeftRadius: 10,}}>
                     <Text style={Headertext.h4}>
@@ -61,7 +91,7 @@ export default Contact = ({ navigation }) => {
                 <Text style={{ fontSize: 17, fontWeight: 'bold', color: Colors.fontColorLightBlack }}>
                     8:00 AM - 5:00 PM
                 </Text>
-            </View>
+            </LRView>
         </SafeAreaView>
     );
 };
@@ -93,7 +123,7 @@ const styles = StyleSheet.create({
     },
     cardEmail: {
         height: 70,
-        backgroundColor: Colors.contactCardColor,
+        backgroundColor: Colors.colorType1_1,
         marginTop: 50,
         marginBottom: 50,
         marginRight: 100,
@@ -107,7 +137,7 @@ const styles = StyleSheet.create({
     },
     cardPhone: {
         height: 70,
-        backgroundColor: Colors.contactCardColor,
+        backgroundColor: Colors.colorType1_1,
         marginBottom: 100,
         marginLeft: 100,
         borderTopLeftRadius: 10,
